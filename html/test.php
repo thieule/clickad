bb187943aabfb0faae23c543e8928fe812b840fb
quangthieuagu <quangthieuagu@9ab81972-60fa-35d9-5674-ea2a608f8a98>
2011-10-21 14:48:53 +0000

    add source

---
+++ html/test.php
@@ -0,0 +1,4 @@
+<div>
+    <h3>Iframe</h3>
+    <a target="_parent" href="http://localhost/click/<?php echo $option ?>/<?php echo $author ?>/1/<?php echo base64_encode('http://vnexpress.net') ?>/">download</a>
+</div>
